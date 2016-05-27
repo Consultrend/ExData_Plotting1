e4127e59d6a922d8706378b4a7aee194baab5ee4
Consultrend <fvandenend@gmail.com>
2016-05-27 11:42:43 +0200

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,11 +1,10 @@
 ## Load the data in R
-
+## Only the neceassary data is loaded into R.
 HPC_feb <- read.csv("E:/Kennis/Coursera/Data scientist/Exploratory Data Analysis/household_power_consumption_feb.txt", header=TRUE, sep="|")
 
-library(datasets)
-?hist
+png("E:/Kennis/Coursera/Data scientist/Exploratory Data Analysis/plot1.png", width=800, height=800)
 
 #Create plot
 hist(HPC_feb$Global_active_power, col="red", main = ("Global Active Power"), xlab="Global active Power(kilowatts", ylab="frequency")
-dev.copy(png,file="plot1.png") #create png
+
 dev.off() #close the file
