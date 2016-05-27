c5f4c20f2248b9892776f749e06e9c39a6c4481c
Consultrend <fvandenend@gmail.com>
2016-05-27 11:43:54 +0200

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,15 +1,14 @@
 ## Load the data in R
+## Only the neceassary data is loaded into R.
 
 HPC_feb <- read.csv("E:/Kennis/Coursera/Data scientist/Exploratory Data Analysis/household_power_consumption_feb.txt", header=TRUE, sep="|",stringsAsFactors=FALSE, dec=".")
 
 
 #Create plot
-Plot2 <- 
+png("E:/Kennis/Coursera/Data scientist/Exploratory Data Analysis/plot2.png", width=800, height=800)
   
 plot(strptime(paste(HPC_feb$Date, HPC_feb$Time, sep=" "), "%d/%m/%Y %H:%M:%S"),as.numeric(HPC_feb$Global_active_power), type="l", xlab="",ylab="Global Active Power (kilowatts)" )
 
-
-dev_copy(png,file="plot2.png") #create png
 dev.off() #close the file
 
 
